c42a995ca4ecda840d3c8235a609d7d85df7c6db
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-20 07:32:27 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+module.exports = {
+  SESSION_ID: '',
+  ALIVE_REACT: '👋',
+  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
+
+⏰ Now time is @time
+📅 Date : @date
+
+🟢 I am online still @uptime
+🔄 Response Delay : @ping Ms
+💻 Host : @host
+🪢 Version : @version
+
+☫ Work Mode : @mode
+☫ Work Type : @worktype
+
+Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
+  ALIVE_MEDIA: 'https://telegra.ph/file/d0fdb53f9f22eefd486cb.jpg',
+  MONGODB_URL: '', 
+  PREFIX: '.$&',
+  LANGUAGE: 'EN',
+  PORT: 3000,
+  BOT_NAME: 'MAXBOT-MD',
+  OWNER_NAME: 'AUSTIN MAX',
+  OWNER_NUMBER: '265997133051',
+  SUDO: '265997133051',
+  WORK_TYPE: 'public',
+  WORK_MODE: 'group',
+  TIME_ZONE: 'Africa/Blantyre',
+  CAPTION: 'SENT BY AUSTIN MAX',
+  STICKER_AUTHOR: 'MAXBOT',
+  STICKER_PACK: 'MAXBOT',
+  REMOVE_BG_APIKEY: '',
+  MAXIMUM_FILESIZE: 90,
+  OWNER_R_EMOJI: '👽',
+  ALIVE_AS_GIF: false,
+  AUTO_VOICE: false,
+  INBOX_BLOCK: false,
+  OWNER_REACT: true,
+  AUTO_REACT: true,
+  ONLY_PM_GROUP_MSG_SEND: false,
+  AUTO_SEEN_STATUS: false,
+  BOT_OFFLINE: true,
+  READ_MESSAGE: false,
+  ANTI_MSG_SPAM: false,
+  ANTI_BAD_WORD: false,
+  ANTI_VIEW_ONES: true,
+  ANTI_DELETE: true,
+  BANED_USER_MSG_SEND: false,
+  BLOCK_CMD_MSG_SEND: false,
+  INBOX_BLOCK_MSG_SEND: false,
+  CMD_CORRECT: true,
+  ANTI_CHANNEL_FORWARD: false,
+  ANTI_BOT: false,
+  BUTTON_MESSAGES: true,
+  SONG_DOWNLOAD_TYPE: 'selectable', // audio , document
+  VIDEO_DOWNLOAD_TYPE: 'selectable', // video
+  FB_DOWNLOAD_TYPE: 'selectable', // hdvideo , sdvideo
+  TIKTOK_DOWNLOAD_TYPE: 'selectable', // nowmvideo , wmvideo
+  MSG: {
+    ONLY_GROUP_MSG: 'Bot only Can Use Groups!',
+    ONLY_PM_MSG: 'Bot only Can Use Private Chats!',
+    BAD_DETECT_MSG: '@user Bad Words Are Disable by Owner!',
+    BANED_MSG: "@user You Can't use bot, You are baned by owner!",
+    GROUP_LEAVE: '👋 Bye @groupName I am Going.',
+    INBOX_BLOCK:
+      '❌ My owner has activated auto inbox blocking. So you will be auto blocked after 8 seconds'
+  },
+  BOT_RULES:
+    "*⚖️ 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂 ⚖️*\n\n☫ Don't spam messages.\n☫ Don't add groups without asking owner.\n"
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright('Update ' + __filename))
+  delete require.cache[file]
+  require(file)
+})
